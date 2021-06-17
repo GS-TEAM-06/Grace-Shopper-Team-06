c2faddac68545046082e807ffaf13de832c3c1af
Sherman Tung <shermantung@outlook.com>
2021-06-17 15:20:10 -0400

    post, put , delete routes for cart

--- server/api/users.js
+++ server/api/users.js
@@ -1,19 +1,19 @@
-const router = require('express').Router();
+const router = require("express").Router();
 const {
   models: { User },
-} = require('../db');
-const Orders = require('../db/models/order');
-const Cards = require('../db/models/Cards');
-const OrderItems = require('../db/models/orderItem');
+} = require("../db");
+const Orders = require("../db/models/order");
+const Cards = require("../db/models/Cards");
+const OrderItems = require("../db/models/orderItem");
 
 // get /api/users/ => return all users
-router.get('/', async (req, res, next) => {
+router.get("/", async (req, res, next) => {
   try {
     const users = await User.findAll({
       // explicitly select only the id and username fields - even though
       // users' passwords are encrypted, it won't help if we just
       // send everything to anyone who asks!
-      attributes: ['id', 'username'],
+      attributes: ["id", "username"],
     });
     res.json(users);
   } catch (err) {
@@ -22,7 +22,7 @@ router.get('/', async (req, res, next) => {
 });
 
 // get /api/users/:id => returns an individual user
-router.get('/:userId', async (req, res, next) => {
+router.get("/:userId", async (req, res, next) => {
   try {
     const user = (await User.findByPk(req.params.userId)).get({ plain: true });
     delete user.password;
@@ -33,7 +33,7 @@ router.get('/:userId', async (req, res, next) => {
 });
 
 // get /api/users/:id/orders => returns an individual user's past orders
-router.get('/:userId/orders', async (req, res, next) => {
+router.get("/:userId/orders", async (req, res, next) => {
   try {
     const orders = (
       await User.findByPk(req.params.userId, {
@@ -54,7 +54,7 @@ router.get('/:userId/orders', async (req, res, next) => {
   }
 });
 
-router.get('/:userId/cart', async (req, res, next) => {
+router.get("/:userId/cart", async (req, res, next) => {
   try {
     const cart = (
       await Orders.findOne({
@@ -65,6 +65,47 @@ router.get('/:userId/cart', async (req, res, next) => {
     res.json(cart);
   } catch (err) {
     next(err);
+  }
+});
+
+//ADD ITEM TO CART
+router.post("/", async (req, res, next) => {
+  try {
+    const singleItem = await OrderIems.create(req.body);
+    res.json(singleItem);
+  } catch (error) {
+    next(error);
+  }
+});
+
+//UPDATE ITEM IN CART
+router.put("/:userId/cart", async (req, res, next) => {
+  try {
+    const cart = (
+      await Orders.findOne({
+        include: [{ model: OrderItems, include: [Cards] }],
+        where: { userId: userId, isOpen: true },
+      })
+    ).get({ plain: true });
+    res.json(await cart.update(req.body));
+  } catch (error) {
+    next(error);
+  }
+});
+
+//DELETE ITEM IN CART
+router.delete("/:userId/cart", async (req, res, next) => {
+  try {
+    const cart = (
+      await Orders.findOne({
+        include: [{ model: OrderItems, include: [Cards] }],
+        where: { userId: userId, isOpen: true },
+      })
+    ).get({ plain: true });
+    await cart.destroy();
+    res.json(cart);
+  } catch (error) {
+    next(error);
   }
 });
 
