4418542888748560498dc045a989d26facb527c3
gytterberg <gytterberg@gmail.com>
2021-06-20 11:00:42 -0400

    update

--- server/api/users.js
+++ server/api/users.js
@@ -1,20 +1,20 @@
-const router = require("express").Router();
+const router = require('express').Router();
 const {
-  models: {User},
-} = require("../db");
-const Orders = require("../db/models/order");
-const Cards = require("../db/models/Cards");
-const OrderItems = require("../db/models/orderItem");
-const {isAuthenticated, isSameUser, isAdmin} = require("../authMiddleware");
+  models: { User },
+} = require('../db');
+const Orders = require('../db/models/order');
+const Cards = require('../db/models/Cards');
+const OrderItems = require('../db/models/orderItem');
+const { isAuthenticated, isSameUser, isAdmin } = require('../authMiddleware');
 
 // get /api/users/ => return all users
-router.get("/", async (req, res, next) => {
+router.get('/', async (req, res, next) => {
   try {
     const users = await User.findAll({
       // explicitly select only the id and username fields - even though
       // users' passwords are encrypted, it won't help if we just
       // send everything to anyone who asks!
-      attributes: ["id", "username"],
+      attributes: ['id', 'username'],
     });
     res.json(users);
   } catch (err) {
@@ -23,17 +23,17 @@ router.get("/", async (req, res, next) => {
 });
 
 // get /api/users/:id => returns an individual user
-router.get("/:userId", isAuthenticated, isSameUser, async (req, res, next) => {
+router.get('/:userId', isAuthenticated, isSameUser, async (req, res, next) => {
   try {
     const user = await User.findByPk(req.params.userId);
     if (user === null) {
-      const error = new Error("Not found!");
+      const error = new Error('Not found!');
       error.status = 404;
       throw error;
     }
 
     delete user.password;
-    res.json(user.get({plain: true}));
+    res.json(user.get({ plain: true }));
   } catch (err) {
     next(err);
   }
@@ -41,13 +41,13 @@ router.get("/:userId", isAuthenticated, isSameUser, async (req, res, next) => {
 
 // get /api/users/:id/orders => returns an individual user's past orders
 router.get(
-  "/:userId/orders",
+  '/:userId/orders',
   isAuthenticated,
   isSameUser,
   async (req, res, next) => {
     try {
       if (req.user.id != req.params.userId) {
-        const error = new Error("Unauthorized!");
+        const error = new Error('Unauthorized!');
         error.status = 401;
         throw error;
       }
@@ -57,19 +57,19 @@ router.get(
         include: [
           {
             model: Orders,
-            where: {isOpen: false},
-            include: [{model: OrderItems, include: [Cards]}],
+            where: { isOpen: false },
+            include: [{ model: OrderItems, include: [Cards] }],
           },
         ],
-        where: {userId: req.params.userId},
+        where: { userId: req.params.userId },
       });
       if (orders === null) {
-        const error = new Error("Not found!");
+        const error = new Error('Not found!');
         error.status = 404;
         throw error;
       }
 
-      res.json(orders.get({plain: true}));
+      res.json(orders.get({ plain: true }));
     } catch (err) {
       next(err);
     }
@@ -77,28 +77,28 @@ router.get(
 );
 
 router.get(
-  "/:userId/cart",
+  '/:userId/cart',
   isAuthenticated,
   isSameUser,
   async (req, res, next) => {
     try {
       if (req.user.id != req.params.userId) {
-        const error = new Error("Unauthorized!");
+        const error = new Error('Unauthorized!');
         error.status = 401;
         throw error;
       }
 
       const cart = await Orders.findOne({
-        include: [{model: OrderItems, include: [Cards]}],
-        where: {userId: req.params.userId, isOpen: true},
+        include: [{ model: OrderItems, include: [Cards] }],
+        where: { userId: req.params.userId, isOpen: true },
       });
       if (cart === null) {
-        const error = new Error("Not found!");
+        const error = new Error('Not found!');
         error.status = 404;
         throw error;
       }
 
-      res.json(cart.get({plain: true}));
+      res.json(cart.get({ plain: true }));
     } catch (err) {
       next(err);
     }
@@ -106,7 +106,7 @@ router.get(
 );
 
 //ADD ITEM TO CART
-router.post("/", isAuthenticated, isSameUser, async (req, res, next) => {
+router.post('/', isAuthenticated, isSameUser, async (req, res, next) => {
   try {
     const singleItem = await OrderItems.create(req.body);
     res.json(singleItem);
@@ -115,50 +115,65 @@ router.post("/", isAuthenticated, isSameUser, async (req, res, next) => {
   }
 });
 
-//UPDATE ITEM IN CART
+//UPDATE ITEM IN CART: cardId, quantity method
 router.put(
-  "/:userId/cart",
+  '/:userId/cart',
   isAuthenticated,
   isSameUser,
   async (req, res, next) => {
     try {
+      // did we get a quantity? Default is to add 1
+      if (typeof req.body.quantity === 'undefined') {
+        req.body.quantity = 1;
+      }
+
       // find the cart instance that matches this user's id
       let cart = await Orders.findOne({
-        include: [{model: OrderItems, include: [Cards]}],
-        where: {userId: req.params.userId, isOpen: true},
+        include: [{ model: OrderItems, include: [Cards] }],
+        where: { userId: req.params.userId, isOpen: true },
       });
-
-      // get a plain object for the cart instance dataValues
-      let plainCart = cart.get({plain: true});
 
       // check to make sure we found a cart
       if (cart === null) {
-        const error = new Error("Cart not found!");
+        const error = new Error('Cart not found!');
         error.status = 404;
         throw error;
       }
+
+      // get a plain object for the cart instance dataValues
+      let plainCart = cart.get({ plain: true });
 
       // get the card instance that matches cardId
       const card = await Cards.findByPk(req.body.cardId);
 
       // check to make sure we found the card
       if (card === null) {
-        const error = new Error("Card not found!");
+        const error = new Error('Card not found!');
         error.status = 404;
         throw error;
       }
 
       // walk through the orderItems in this cart
-      // if the card is already in the cart, increment it's quantity
+      // if the card is already in the cart, change it's quantity
+      // if the quantity is 0, delete the orderItem
       let orderItem = false;
       for (let i = 0; i < plainCart.orderItems.length; i++) {
         if (
           Number(plainCart.orderItems[i].cardId) === Number(req.body.cardId)
         ) {
-          orderItem = await OrderItems.update(
-            {quantity: plainCart.orderItems[i].quantity + 1},
-            {where: {cardId: plainCart.orderItems[i].cardId}}
-          );
+          // if quantity is 0 we need to actually delete the line
+          if (Number(req.body.quantity) === 0) {
+            orderItem = await OrderItems.destroy({
+              where: { id: plainCart.orderItems[i].id },
+            });
+          } else {
+            orderItem = await OrderItems.update(
+              {
+                quantity: req.body.quantity,
+              },
+              { where: { cardId: plainCart.orderItems[i].cardId } }
+            );
+          }
 
           // if for some reason we didn't update anything, throw a 500 error
           if (orderItem[0] === 0) {
@@ -169,7 +184,7 @@ router.put(
         }
       }
 
-      // if we get here with updated === false, we didn't have the card in the cart already
+      // if we get here with orderItem === false, we didn't have the card in the cart already
       if (orderItem === false) {
         // create new OrderItem with cardId = req.body.cardId
         orderItem = await OrderItems.create({
@@ -181,8 +196,8 @@ router.put(
 
       // get a fresh instance of the cart
       cart = await Orders.findOne({
-        include: [{model: OrderItems, include: [Cards]}],
-        where: {userId: req.params.userId, isOpen: true},
+        include: [{ model: OrderItems, include: [Cards] }],
+        where: { userId: req.params.userId, isOpen: true },
       });
 
       // send it back, in JSON.stringify format! (this is very confusing that sequelize magically does this)
@@ -195,23 +210,23 @@ router.put(
 
 //DELETE ITEM IN CART
 router.delete(
-  "/:userId/cart",
+  '/:userId/cart',
   isAuthenticated,
   isSameUser,
   async (req, res, next) => {
     try {
       // find the cart instance that matches this user's id
       let cart = await Orders.findOne({
-        include: [{model: OrderItems, include: [Cards]}],
-        where: {userId: req.params.userId, isOpen: true},
+        include: [{ model: OrderItems, include: [Cards] }],
+        where: { userId: req.params.userId, isOpen: true },
       });
 
       // get a plain object for the cart instance dataValues
-      let plainCart = cart.get({plain: true});
+      let plainCart = cart.get({ plain: true });
 
       // check to make sure we found a cart
       if (cart === null) {
-        const error = new Error("Cart not found!");
+        const error = new Error('Cart not found!');
         error.status = 404;
         throw error;
       }
@@ -219,19 +234,17 @@ router.delete(
       // walk through the orderItems in this cart
       // if the card quantity is greater than one, decrement the quantity
       for (let i = 0; i < plainCart.orderItems.length; i++) {
-        if (
-          plainCart.orderItems[i].cardId === req.body.cardId
-        ) {
+        if (plainCart.orderItems[i].cardId === req.body.cardId) {
           if (plainCart.orderItems[i].quantity > 1) {
             await OrderItems.update(
-              {quantity: plainCart.orderItems[i].quantity - 1},
-              {where: {cardId: plainCart.orderItems[i].cardId}}
+              { quantity: plainCart.orderItems[i].quantity - 1 },
+              { where: { cardId: plainCart.orderItems[i].cardId } }
             );
 
             // else if card quantity is only 1, delete the orderItem
           } else if (plainCart.orderItems[i].quantity === 1) {
             await OrderItems.destroy({
-              where: {cardId: plainCart.orderItems[i].cardId}
+              where: { cardId: plainCart.orderItems[i].cardId },
             });
           }
         }
@@ -239,8 +252,8 @@ router.delete(
 
       // get a fresh instance of the cart
       cart = await Orders.findOne({
-        include: [{model: OrderItems, include: [Cards]}],
-        where: {userId: req.params.userId, isOpen: true},
+        include: [{ model: OrderItems, include: [Cards] }],
+        where: { userId: req.params.userId, isOpen: true },
       });
       res.json(cart);
     } catch (error) {
